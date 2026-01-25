bc915e88f493ca330a338edfb691caa5d375f0ba
Osama Saeed <osamas.bizz@gmail.com>
2026-01-25 15:15:46 +0500

    Enhance title and description in app.py
    Updated the title and added developer information in the description.

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ st.set_page_config(page_title="Smishing Detector", page_icon="🛡️", layout="
 # 1. TITLE AND DESCRIPTION
 st.title("🛡️ AI Smishing Detector")
 st.markdown("""
-This AI is trained on the **SMS Spam Collection** dataset. 
+This AI Model is developed by **Osama Saeed** and is trained on the **SMS Spam Collection** dataset.
+For feedback and suggestion, please Email: osamas.bizz@gmail.com
 Enter a text message below to check if it's **Safe (Ham)** or **Malicious (Spam/Smishing)**.
 """)
 
